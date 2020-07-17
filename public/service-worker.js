78d060b7a61a3fad7e822a053c32029d3245859e
Glitch (exclusive-cooing-land) <none>
2020-07-17 14:07:14 +0000

    🏦📖 Checkpoint ./public/service-worker.js:18993633/4476

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,32 +15,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-'use strict';
+"use strict";
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = "static-cache-v1";
 
 // CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-  '/offline.html',
-];
+const FILES_TO_CACHE = ["/offline.html"];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
+self.addEventListener("install", evt => {
+  console.log("[ServiceWorker] Install");
   // CODELAB: Precache static resources here.
 
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
+self.addEventListener("activate", evt => {
+  console.log("[ServiceWorker] Activate");
 
+  // CODELAB: Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
+self.addEventListener("fetch", evt => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
 
+  // CODELAB: Add fetch event handler here.
+  if (evt.request.mode !== "navigate") {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open(CACHE_NAME).then(cache => {
+        return cache.match("offline.html");
+      });
+    })
+  );
 });
